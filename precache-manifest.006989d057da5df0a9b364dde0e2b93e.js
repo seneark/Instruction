ba064b3b893d38127e100ddfb399ed48a7853fce
seneark <seneark@protonmail.com>
2020-08-27 15:50:47 +0530

    Updates

---
+++ precache-manifest.006989d057da5df0a9b364dde0e2b93e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f0c918863340cf15a3c885d9430c36",
+    "url": "/Instruction/index.html"
+  },
+  {
+    "revision": "da2d74b8ba1af5205a5e",
+    "url": "/Instruction/static/css/2.13ccdce4.chunk.css"
+  },
+  {
+    "revision": "708dd61e407d6614c864",
+    "url": "/Instruction/static/css/main.e0031b11.chunk.css"
+  },
+  {
+    "revision": "da2d74b8ba1af5205a5e",
+    "url": "/Instruction/static/js/2.2e1c3b7f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Instruction/static/js/2.2e1c3b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708dd61e407d6614c864",
+    "url": "/Instruction/static/js/main.eb863a80.chunk.js"
+  },
+  {
+    "revision": "59452cffa2ca2df041c5",
+    "url": "/Instruction/static/js/runtime-main.6c0081a6.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Instruction/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Instruction/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Instruction/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Instruction/static/media/notification.c0d3c94c.eot"
+  }
+]);
